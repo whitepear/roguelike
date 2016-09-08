71162c958d594f886df4f287622cfb84ac5b292b
John Doe <webdevnorth@gmail.com>
2016-09-08 21:35:12 +0100

    index.js boilerplate implemented

---
+++ app/index.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+// var MainContainer = require('./containers/MainContainer.js');
+require('./styles/app.scss');
+
+ReactDOM.render(
+	// <MainContainer />,
+	document.getElementById('app')
+);
